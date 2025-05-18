53495da49e39e262d023ef4d23a712b6ed1c1543
kushal-10 <kushalkoshti11@gmail.com>
2025-05-18 15:46:28 +0200

    add semantic filter

---
+++ src/retrievalv2/semantic_filter.py
@@ -0,0 +1,95 @@
+"""
+USE GPT-4.1-mini to classify if the extracted passage actually mentions AI or related terms,
+Some passages may mention "AI" in a different context, ref LNT 2015 for an example
+
+Cost Analysis - IP token = 1.24M approx
+
+MODEL     IP      OP
+4.1mini   0.5     0.2       0.7$   1.4
+4.1       2.5     1         3.5$   10.5
+
+"""
+
+from openai import OpenAI
+import os
+import json
+from tqdm import tqdm
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename=os.path.join("src", "retrievalv2", "semantic_filter.log"),
+                    filemode='w')
+
+client = OpenAI()
+BASE_PROMPT = """
+Given the following PASSAGE, classify it YES if it mentions Artificial Intelligence or related technologies. If it does not then classify
+it NO. Also give a short one line explanation for your classification.
+Provide the output in the following format:
+{
+  "Classification": YES or NO,
+  "Explanation": One line explanation for your classification.
+}
+Here's the PASSAGE : 
+"""
+
+
+def classify(input_prompt: str):
+    completion = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "system", "content": ""},
+            {
+                "role": "user",
+                "content": BASE_PROMPT + input_prompt
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content
+
+
+def get_classifications(BASE_DIR: str = "annual_txts_fitz", save_name: str = "semantic_output.json"):
+    """
+    Finegrained Filter for AI related passages from regex_output.json
+    Saves filtered chunks in save_name.json under same folder as regex_output.json
+    """
+
+    regex_outputs = []
+    for dirname, _, filenames in os.walk(BASE_DIR):
+        for filename in filenames:
+            if filename.endswith("regex_output.json"):
+                regex_outputs.append(os.path.join(dirname, filename))
+
+
+    for regex_output in tqdm(regex_outputs):
+        with open(regex_output) as f:
+            regex_data = json.load(f)
+            chunks = regex_data["chunks"]
+
+        # Create a save file for semantic filter outputs
+        save_path = regex_output.replace("regex_output.json", "semantic_output.json")
+        if os.path.exists(save_path):
+            logging.info(f"Skipping {regex_output}")
+            continue # Process only new files
+        else:
+            logging.info(f"Writing {regex_output}")
+            semantic_data = []
+            for chunk in chunks:
+                classification = classify(chunk)
+                semantic_data.append({
+                    "chunk": chunk,
+                    "classification": classification,
+                })
+
+            with open(save_path, "w") as f:
+                json.dump(semantic_data, f, indent=4)
+
+
+if __name__ == '__main__':
+
+    get_classifications()
+
+
+
